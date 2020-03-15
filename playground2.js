aa58737fa88690eb842143fab850a65e20aa0bb8
Trey <yosoytrey@users.noreply.github.com>
2020-03-15 15:45:02 -0600

    Add files via upload

---
+++ playground2.js
@@ -0,0 +1,36 @@
+
+/*
+function retirement(retirementAge) {
+    var a = ' years left until retirement.';
+    return function(yearOfBirth) {
+        var age = 2016 - yearOfBirth;
+        console.log((retirementAge - age) + a);
+    }
+}
+
+var retirementUS = retirement(66);
+// var retirementGermany = retirement(65);
+// var retirementIceland = retirement(67);
+
+console.log(typeof(retirementUS))
+*/
+
+console.log('test')
+
+/*
+retirementGermany(1990);
+retirementUS(1990);
+retirementIceland(1990);
+
+
+function retirement(retirementAge) {
+    let a = ' a years left until retirement.'
+
+
+    return function(yearOfBirth) {
+        let age = 2020 - yearOfBirth
+        console.log((retirementAge - age) + a)
+    }
+}
+
+*/
