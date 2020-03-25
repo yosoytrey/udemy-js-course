1c1c12be6e95f2ed1f47cd31fedcff6140bff396
Trey Richardson <trey.richardson@gmail.com>
2020-03-25 16:45:11 -0600

    clean up folder

--- playground2.js
+++ playground2.js
@@ -1,31 +1,15 @@
 
-/*
-function retirement(retirementAge) {
-    var a = ' years left until retirement.';
-    return function(yearOfBirth) {
-        var age = 2016 - yearOfBirth;
-        console.log((retirementAge - age) + a);
-    }
-}
-
 var retirementUS = retirement(66);
-// var retirementGermany = retirement(65);
-// var retirementIceland = retirement(67);
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
 
-console.log(typeof(retirementUS))
-*/
-
-console.log('test')
-
-/*
 retirementGermany(1990);
 retirementUS(1990);
 retirementIceland(1990);
 
 
 function retirement(retirementAge) {
-    let a = ' a years left until retirement.'
-
+    let a = ' years left until retirement.'
 
     return function(yearOfBirth) {
         let age = 2020 - yearOfBirth
@@ -33,4 +17,4 @@ function retirement(retirementAge) {
     }
 }
 
-*/
+
